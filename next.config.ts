52d1132d1a7413c569a13681699907bbb17d442f
Przemysław Onak <przemekonak@gmail.com>
2025-05-05 00:09:18 +0200

    Add CI/CD

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* base path is replced in CI */
+  basePath: ''
 };
 
 export default nextConfig;
